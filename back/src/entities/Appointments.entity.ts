11e4faf153e5150e81fff13a60c3c7159141961e
JFernandezBalza <joss.jota21@gmail.com>
2025-05-26 04:40:10 -0400

    Modificacion de entidad

--- back/src/entities/Appointments.entity.ts
+++ back/src/entities/Appointments.entity.ts
@@ -10,19 +10,19 @@ import {
 import { User } from "./User.entity";
 import { Status } from "../interface/IAppointments";
 
-@Entity("appointments")
+@Entity('appointments')
 export class Appointment {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column({ type: "date", nullable: false })
+  @Column({ type: 'date', nullable: false })
   date: Date;
 
-  @Column({ type: "varchar", length: 5, nullable: false })
+  @Column({ type: 'varchar', length: 5, nullable: false })
   time: string;
 
   @Column({
-    type: "varchar",
+    type: 'varchar',
     length: 10,
     nullable: false,
     default: Status.active,
@@ -37,5 +37,5 @@ export class Appointment {
   createdAt?: Date;
 
   @UpdateDateColumn()
-  update?: Date;
+  updatedAt?: Date;
 }
